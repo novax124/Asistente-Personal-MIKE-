96c7a7fbf4f23d126dba500fff56293d9e3f6f7b
David Puertas <dpue303@ibellvitge.net>
2021-07-25 18:42:28 +0200

    Update CALENDAR_Añadir_Citas.py

--- GOOGLE/CALENDAR/CALENDAR_Añadir_Citas.py
+++ GOOGLE/CALENDAR/CALENDAR_Añadir_Citas.py
@@ -15,48 +15,44 @@ calendar_id_chicago = 'id del calendar'
 Create an event
 """
 colors = service.colors().get().execute()
-#pprint(colors)
+pprint(colors)  #no es necesario hacer este print
 
 
-recurrence = [    #cambiar datos
-    "RRULE:FREQ=MONTHLY;COUNT=2" 
-]
-
-hour_adjustment = -8
+hour_adjustment = -2
 event_request_body = {
     'start': {
         "dateTime": convert_to_RFC_datetime(2021, 5, 21, 12 + hour_adjustment ,45 ),
-        "timeZone": "Asia/Taipei"
+        "timeZone": "zona horaria"
     },
     "end": {
         "dateTime": convert_to_RFC_datetime(2021,5,21,14 + hour_adjustment , 45 ),
-        "timeZone": "Asia/Taipei"
+        "timeZone": "zona horaria"
     },
-    "summary": "Family Lunch",
-    "description": "Having lunch with the parents",
+    "summary": "título del evento",
+    "description": "descripción del evento",
     "colorId": 5,
     "status": "confirmed",
     "transparency": "opaque",
     "visibility": "private",
-    "location": "Chicago, IL",
+    "location": "localidad",
     "attachments": [
         {
-            "fileUrl": "https://drive.google.com/file/d/1GnmQHK6EgcEwHHoOyFREGcgBrstYs_9J/view",
-            "title": "Invitation Letter Template in Word Doc"
+            "fileUrl": "url de documento de drive",
+            "title": "título del documento de drive"
         }
     ],
     "attendees": [
         {
-            "displayName": "JJ",
-            "comment": "I enjoy coding",
-            "email": "dpue303@ibellvitge.net",
+            "displayName": "nombre",
+            "comment": "comentario",
+            "email": "correo electrónico",
             "optional": False,
             "organizer": True,
             "responseStatus": "accepted"
         }
     
     ],
-    "recurrence": recurrence  #datos nuevos
+   
 
 }
 
@@ -79,19 +75,3 @@ pprint(response)
 
 eventId = response["id"]
 
-"""
-Update Data
-"""
-"""
-start_datetime = convert_to_RFC_datetime(2021, 5, 20, 12 + hour_adjustment ,45 )
-end_datetime =  convert_to_RFC_datetime(2021,5,20,14 + hour_adjustment , 45 )
-response["start"]["datetime"] = start_datetime
-response["start"]["datetime"] = end_datetime
-response["summary"] = "Family Dinner"
-response["Description"] = "Having Family Dinner"
-service.events().update(
-    calendarId = calendar_id_chicago,
-    eventId = eventId,
-    body = response).execute()
-
-"""
