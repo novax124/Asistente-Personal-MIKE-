08f6cf65a73b25dbe259cb955c7d6b5069016d32
David Puertas <dpue303@ibellvitge.net>
2021-07-25 19:39:26 +0200

    Update traductor.py

--- GOOGLE/TRADUCTOR/traductor.py
+++ GOOGLE/TRADUCTOR/traductor.py
@@ -1,20 +1,5 @@
-import googletrans
 from googletrans import Translator
 
-#print(googletrans.LANGUAGES)
-
-text1 = "hello, how are you?"
-
-text2 = "hola ¿Cómo estás?"
-
-text3 = "hola, com estàs?"
-
 translator = Translator()
-"""
-print(translator.detect(text1))
-print(translator.detect(text2))
-print(translator.detect(text3))
-"""
-#print("Translated  From Spanish : ", translator.translate(text2))
 
-print("Translate ES to EN : ", translator.translate(text1, src = "en", dest = "es"))
+print(translator.translate("Me gustan los libros", dest = "de" ))
