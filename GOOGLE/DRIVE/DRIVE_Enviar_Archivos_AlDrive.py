b909863e2f8301b8b3b1a753c9309a8ac044cf5f
David Puertas <dpue303@ibellvitge.net>
2021-07-28 22:54:28 +0200

    Update DRIVE_Enviar_Archivos_AlDrive.py

--- GOOGLE/DRIVE/DRIVE_Enviar_Archivos_AlDrive.py
+++ GOOGLE/DRIVE/DRIVE_Enviar_Archivos_AlDrive.py
@@ -8,20 +8,22 @@ SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-folder_id = "id de la carpeta"
-file_names = ["nombre del archivo"]
-mime_types = ["image/jpeg   <-- en el caso de que se una imagen"]
+id_carpeta = "id de la carpeta"
+nom_arxius = ["nom de l'arxiu"]
+tipus = ["image/jpeg   <-- en cas de que sigui una imatge tipus jpg"]
 
-for file_name, mime_type in zip(file_names, mime_types):
-    file_metadata = {
-        "name" : file_name,
-        "parents" : [folder_id]
+for nom_arxiu, tip in zip(nom_arxius, tipus):
+    metadata = {
+        "name" : nom_arxiu,
+        "parents" : [id_carpeta]
     }
 
-    media = MediaFileUpload("./Drive/{0}".format(file_name), mimetype=mime_type)
+    media = MediaFileUpload("./Drive/{0}".format(nom_arxiu), mimetype=tip)
 
     service.files().create(
-        body=file_metadata,
+        body= metadata,
         media_body = media,
         fields = "id"
     ).execute()
+
+    
