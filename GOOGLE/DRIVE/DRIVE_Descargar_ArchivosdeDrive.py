63acb2a180b4610475c8162ee9dcaa858c47f18d
David Puertas <dpue303@ibellvitge.net>
2021-07-28 21:19:10 +0200

    Update DRIVE_Descargar_ArchivosdeDrive.py

--- GOOGLE/DRIVE/DRIVE_Descargar_ArchivosdeDrive.py
+++ GOOGLE/DRIVE/DRIVE_Descargar_ArchivosdeDrive.py
@@ -8,24 +8,24 @@ API_NAME = "drive"
 API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+servei = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-file_ids = ["id del archivo"]
-file_names = ["nombre del archivo"]
+id_arxius = ["id de l'arxiu"]
+nom_arxius = ["nom de l'arxiu"]
 
-for file_id, file_name in zip(file_ids, file_names):
-    request = service.files().get_media(fileId=file_id)
+for id_arxiu, nom_arxiu in zip(id_arxius, nom_arxius):
+    request = servei.files().get_media(fileId=id_arxiu)
 
     fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fd = fh, request=request)
-    done = False
+    descarregar = MediaIoBaseDownload(fd = fh, request=request)
+    acabat = False
 
-    while not done:
-        status, done = downloader.next_chunk()
+    while not acabat:
+        status, acabat = descarregar.next_chunk()
         print("Download progress {0}".format(status.progress() * 100))
 
     fh.seek(0)
 
-    with open(os.path.join("./Photos", file_name), "wb") as f:
+    with open(os.path.join("./Photos", nom_arxiu), "wb") as f:
         f.write(fh.read())
         f.close()
