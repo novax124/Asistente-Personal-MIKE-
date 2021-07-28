f82a9ba73e05cad19c3097dbe19b90d0e19b38c8
David Puertas <dpue303@ibellvitge.net>
2021-07-28 20:28:34 +0200

    Update DRIVE_Copiar_Archivos.py

--- GOOGLE/DRIVE/DRIVE_Copiar_Archivos.py
+++ GOOGLE/DRIVE/DRIVE_Copiar_Archivos.py
@@ -6,20 +6,19 @@ API_NAME = "drive"
 API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+servei = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-source_file_id = "id del archivo"         
-folder_ids = ["id de la carpeta"]           
+id_arxiu = "id del archivo"         
+id_carpeta = ["id de la carpeta"]           
 
-file_metadata = {
+metadata = {
     "name" : "nombre para el archivo",
-    "parents" : folder_ids,
-    "starred" : True,
+    "parents" : id_carpeta,
     "description" : "descripción del archivo"
 
 }
 
-service.files().copy(
-    fileId= source_file_id,
-    body=file_metadata
+servei.files().copy(
+    fileId= id_arxiu,
+    body= metadata
 ).execute()
