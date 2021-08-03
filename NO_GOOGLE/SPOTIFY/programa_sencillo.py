d75172b8eb27d279d5db708a923287225924bd5c
David Puertas <dpue303@ibellvitge.net>
2021-08-03 16:05:21 +0200

    Update programa_sencillo.py

--- NO_GOOGLE/SPOTIFY/programa_sencillo.py
+++ NO_GOOGLE/SPOTIFY/programa_sencillo.py
@@ -1,41 +1,38 @@
+# shows artist info for a URN or URL
+
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
-import webbrowser as web
+import sys
+import pprint
+import webbrowser
 import pyautogui
 from time import sleep
 
-# your credentials
-client_id = 'Poner id' #https://developer.spotify.com/dashboard/login
-client_secret = 'Poner id '
-flag = 0
 
-# artist and name of the song
-author = ''
-song = 'Honeypie'.upper()
+flag = 0
+client_id = "poner id cliente"
+client_secret = "poner id cliente secret"
+author = '' 
+song = "In Too Deep".upper()
+
 
 if len(author) > 0:
-    # authenticate
+
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
     result = sp.search(author)
 
     for i in range(0, len(result["tracks"]["items"])):
-        # songs by artist
         name_song = result["tracks"]["items"][i]["name"].upper()
-
         if song in name_song:
             flag = 1
-            web.open(result["tracks"]["items"][i]["uri"])
+            webbrowser.open(result["tracks"]["items"][i]["uri"])
             sleep(5)
             pyautogui.press("enter")
-            
-# if song by artist not found
-if flag == 0:
-    song = song.replace(" ", "%20")
-    web.open(f'spotify:search:{song}')
-    sleep(5)
-    for i in range(28):                #saltos para llegar a la canción
-        pyautogui.press("tab")
 
-    for i in range(2):
-        pyautogui.press("enter")
-        sleep(600)
+if flag == 0:
+    song= song.replace(" ", "%20")
+    webbrowser.open(f'spotify:search:{song}')
+    sleep(5)
+    for i in range(28):
+        pyautogui.press("tab")  
+    pyautogui.press("enter")
