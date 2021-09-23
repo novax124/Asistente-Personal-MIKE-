a7f6dae24fa977b99567ee1a7ac358c8105095d2
David Puertas <dpue303@ibellvitge.net>
2021-09-23 19:08:12 +0200

    Create blocdenotas.py

---
+++ INTERFAZ_PROGRAMA/PROGRAMAS/blocdenotas.py
@@ -0,0 +1,65 @@
+import speech_recognition as sr
+import pyttsx3, pywhatkit
+from time import gmtime, strftime
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            pc = listener.listen(source)
+            rec = listener.recognize_google(pc, language='es-ES')
+            rec = rec.lower(0)
+
+
+    except:
+        pass
+    return rec
+
+def runn():  #importar esta función para la interfaz gráfica
+               
+                
+                talk("¿Título?")
+                rec = listen()
+
+                if "" in rec:
+                    archi1 = open(rec, "w")
+                    archi1.write(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())+"\n")
+                    talk("¿Qué quieres poner?")
+                    rec = listen()
+
+                    if "" in rec:
+                        archi1.write(rec+"\n")
+                        while True:
+                            talk("¿Algo más?")
+                            rec = listen()
+
+                            if "si" in rec:
+                                talk("Escuchando...")
+                                rec = listen()
+                                if "" in rec:
+                                    archi1.write(rec+"\n")
+
+                            if "no" in rec:
+                                archi1.close()
+                                talk("Bloc cerrado")
+                                break
+                            
+                        
+
+                else:
+                    print(rec)
+                    pass
+
+
+
