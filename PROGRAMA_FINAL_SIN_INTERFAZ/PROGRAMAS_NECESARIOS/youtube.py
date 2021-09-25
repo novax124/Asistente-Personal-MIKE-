bb469a185c94764c1c922ea6bc94012ba2969cef
David Puertas <dpue303@ibellvitge.net>
2021-09-25 15:02:07 +0200

    Create youtube.py

---
+++ PROGRAMA_FINAL_SIN_INTERFAZ/PROGRAMAS_NECESARIOS/youtube.py
@@ -0,0 +1,45 @@
+import webbrowser
+import pyautogui    
+from time import sleep
+
+import speech_recognition as sr
+import pyttsx3 
+
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            pc = listener.listen(source)
+            rec = listener.recognize_google(pc, language='es-ES')
+            rec = rec.lower(0)
+    except:
+        pass
+    return rec
+
+def run_mike3(): #importar esta función para la interfaz gráfica
+
+    talk("¿Quieres buscar algo en cocreto?")
+    rec = listen()
+    if "no quiero" in rec:
+        webbrowser.open("https://www.youtube.com/")
+    else:
+        webbrowser.open("https://www.youtube.com/results?search_query=" + str(rec))
+        sleep(3)   
+        pyautogui.press("tab")
+        pyautogui.press("enter")
+
+
+run_mike3()
