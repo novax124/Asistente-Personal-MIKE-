23b0832c3dba519c9ebb69b9656ad9d927e44dde
David Puertas <dpue303@ibellvitge.net>
2021-09-25 15:02:44 +0200

    Create grabadora.py

---
+++ PROGRAMA_FINAL_SIN_INTERFAZ/PROGRAMAS_NECESARIOS/grabadora.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+
+engine = pyttsx3.init() 
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            pc = listener.listen(source)
+            rec = listener.recognize_google(pc, language='es-ES')
+            rec = rec.lower(0)
+
+
+    except:
+        pass
+    return rec
+
+def run_mike9(): #importar esta función para la interfaz gráfica
+    talk("¿Título de la grabación?")
+    rec = listen()
+    tit = rec
+    talk("Te escucho")
+    with sr.Microphone() as source:
+        audio = listener.listen(source)
+
+        with open(tit + ".wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+    talk("Grabación finalizada")
+
+
+run_mike9()
